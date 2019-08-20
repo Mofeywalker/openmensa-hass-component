4911f96975c475bc13b084d2df876f6e9a49aae7
Moritz Fey <moritz.fey@gmail.com>
2019-08-20 10:17:14 +0200

    reqork sensor
    closes #1

--- custom_components/openmensa/sensor.py
+++ custom_components/openmensa/sensor.py
@@ -1,11 +1,12 @@
 
 import logging
 from datetime import timedelta, datetime
+import unicodedata
 
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_CODE, CONF_HOST, STATE_UNAVAILABLE
+from homeassistant.const import CONF_CODE, STATE_UNAVAILABLE
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util import Throttle
@@ -14,14 +15,6 @@ _LOGGER = logging.getLogger(__name__)
 
 CONF_DEFAULT_MENSA = 828
 MEAL_CATEGORY = "category"
-MEAL_CATEGORY_KOMPLETT = "Komplett Menü"
-MEAL_CATEGORY_VEGETARISCH = "Vegetarisches Menü"
-MEAL_CATEGORY_WAHLESSEN = "Wahlessen"
-
-ATTR_KOMPLETT_MENU = "komplett_menu"
-ATTR_VEGETARISCHES_MENU = "vegetarisches_menu"
-ATTR_WAHLESSEN = "wahlessen_"
-MAX_WAHLESSEN_COUNT = 4
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
 CONST_NAME = "name"
@@ -37,7 +30,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     from openmensa import OpenMensa as om
-    _LOGGER.debug("in setup methode")
+    _LOGGER.debug("setup openmensa sensor")
     mensa = config.get(CONF_CODE)
     add_entities([OpenmensaSensor(om, mensa)], True)
 
@@ -49,56 +42,47 @@ class OpenmensaSensor(Entity):
         self._om = om
         self._mensa = mensa
         self._state = STATE_UNAVAILABLE
-
-        self._komplett_menu = None
-        self._vegetarisches_menu = None
-        self._wahlessen = None
+        self._essen = None
 
     def get_meals_of_the_day(self):
         date_str = datetime.now().strftime("%Y-%m-%d")
         meals = self._om.get_meals_by_day(self._mensa, date_str)
-        komplett_menu = None
-        vegetarisches_menu = None
-        wahlessen = []
+        essen = {}
         for meal in meals:
-            if meal[MEAL_CATEGORY] == MEAL_CATEGORY_KOMPLETT:
-                komplett_menu = meal[CONST_NAME]
-            if meal[MEAL_CATEGORY] == MEAL_CATEGORY_VEGETARISCH:
-                vegetarisches_menu = meal[CONST_NAME]
-            if meal[MEAL_CATEGORY] == MEAL_CATEGORY_WAHLESSEN:
-                wahlessen.append(meal[CONST_NAME])
-        return (komplett_menu, vegetarisches_menu, wahlessen)
+            meal_category = self.normalize_string(meal[MEAL_CATEGORY])
+            if meal_category not in essen:
+                essen[meal_category] = []
+            essen[meal_category].append({"name": meal[CONST_NAME]})
+        return essen
+
+    def normalize_string(self, my_string):
+        """
+        Normalize a String with Umlauts and blanks to ascii, lowercase and _ instead of blanks.
+        :param my_string:
+        :return:
+        """
+        return unicodedata.normalize('NFKD', my_string).encode('ASCII', 'ignore').decode().replace(" ", "_").lower()
 
     @property
     def name(self):
-        """Return the name of the sensor."""
         return 'Openmensa Sensor'
 
     @property
     def icon(self):
-        """Icon to use in the frontend, if any."""
         return ICON
 
     @property
     def state(self):
-        """Return the state of the sensor."""
         return self._state
 
     @property
     def state_attributes(self):
         if self._state == STATE_UNAVAILABLE:
             return {}
-        attr = {
-            ATTR_KOMPLETT_MENU: self._komplett_menu,
-            ATTR_VEGETARISCHES_MENU: self._vegetarisches_menu,
-        }
-        # There can be multiple Wahlessen and the number of meals is mostly random
-        # check if a meal exists, otherwise set to None
-        for idx in range(MAX_WAHLESSEN_COUNT):
-            try:
-                attr[ATTR_WAHLESSEN + str(idx)] = self._wahlessen[idx]
-            except IndexError:
-                attr[ATTR_WAHLESSEN + str(idx)] = None
+        attr = {}
+        for category in self._essen:
+            attr[category] = self._essen[category]
+
         return attr
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
@@ -106,8 +90,5 @@ class OpenmensaSensor(Entity):
         """Fetch new meals for the day
         """
         _LOGGER.debug("Updating meals of the day.")
-        komplett, vegetarisch, wahlessen = self.get_meals_of_the_day()
-        self._komplett_menu = komplett
-        self._vegetarisches_menu = vegetarisch
         self._state = STATE_ONLINE
-        self._wahlessen = wahlessen
+        self._essen = self.get_meals_of_the_day()
